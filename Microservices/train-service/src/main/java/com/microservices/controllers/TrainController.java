0669df301c22fc9fbacc2ea90ce02fb4fa89f06e
pankaj143p <pankaj114477pankaj@gmail.com>
2025-04-05 14:39:44 +0530

    add train method added

--- Microservices/train-service/src/main/java/com/microservices/controllers/TrainController.java
+++ Microservices/train-service/src/main/java/com/microservices/controllers/TrainController.java
@@ -1,12 +1,19 @@
 package com.microservices.controllers;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.microservices.model.TrainDetails;
+import com.microservices.service.TrainServie;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/train")
+@RequiredArgsConstructor
 public class TrainController {
 
-    @GetMapping("")
-    public String addTrain(){
-        return "Train will be start";
+    private final TrainServie trainServie;
+    @PostMapping("/add")
+    public ResponseEntity<String> addTrain(@RequestBody TrainDetails req){
+        String newTrain = trainServie.addTrain(req);
+        return ResponseEntity.ok(newTrain);
     }
 }
--- Microservices/train-service/src/main/java/com/microservices/service/implementation/TrainImplementation.java
+++ Microservices/train-service/src/main/java/com/microservices/service/implementation/TrainImplementation.java
@@ -1,16 +1,34 @@
 package com.microservices.service.implementation;
 
 import com.microservices.model.TrainDetails;
+import com.microservices.repository.TrainRepository;
 import com.microservices.service.TrainServie;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class TrainImplementation implements TrainServie {
+
+    private final TrainRepository trainRepository;
+
     @Override
-    public String addTrain(TrainDetails train) {
-        return "";
+    public String addTrain(TrainDetails req) {
+        TrainDetails train = new TrainDetails();
+        train.setTrainName(req.getTrainName());
+        train.setTotalSeats(req.getTotalSeats());
+        train.setDestination(req.getDestination());
+        train.setSource(req.getSource());
+        train.setRoutes(req.getRoutes());
+        train.setArrivalTime(req.getArrivalTime());
+        train.setDepartureTime(req.getDepartureTime());
+        trainRepository.save(train);
+        return "Train added successfully";
     }
 
     @Override
     public String updateTrain(TrainDetails train, Long train_id) {
+
         return "";
     }
 }
