34aad09686bd22d63d3dd3ec2df36a3827e45827
pankaj143p <pankaj114477pankaj@gmail.com>
2025-04-05 14:08:44 +0530

    service interface and class added

---
+++ Microservices/train-service/src/main/java/com/microservices/service/TrainServie.java
@@ -0,0 +1,8 @@
+package com.microservices.service;
+
+import com.microservices.model.TrainDetails;
+
+public interface TrainServie {
+    String addTrain(TrainDetails train);
+    String updateTrain(TrainDetails train, Long train_id);
+}
---
+++ Microservices/train-service/src/main/java/com/microservices/service/implementation/TrainImplementation.java
@@ -0,0 +1,16 @@
+package com.microservices.service.implementation;
+
+import com.microservices.model.TrainDetails;
+import com.microservices.service.TrainServie;
+
+public class TrainImplementation implements TrainServie {
+    @Override
+    public String addTrain(TrainDetails train) {
+        return "";
+    }
+
+    @Override
+    public String updateTrain(TrainDetails train, Long train_id) {
+        return "";
+    }
+}
