d2d0b88f2aa79f37139ab8719b27d4cbe451186b
pankaj143p <pankaj114477pankaj@gmail.com>
2025-07-06 17:04:07 +0530

    build changed in json files

--- railway-reservation/vite.config.ts
+++ railway-reservation/vite.config.ts
@@ -4,12 +4,23 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@': '/src',
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          router: ['react-router-dom'],
+          ui: ['@mui/material', '@mui/icons-material'],
+          utils: ['axios', 'jwt-decode']
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000
+  }
 })
